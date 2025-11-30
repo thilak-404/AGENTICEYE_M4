1e31d0fd1d745f3423d1d3442113a19aba17c95d
Thilak Divyadharshan <thilakdivyadharshan@karunya.edu.in>
2025-11-30 11:49:23 +0530

    Move helper Python files to _lib to prevent Vercel function creation

--- frontend/api/py/index.py
+++ frontend/api/py/index.py
@@ -2,9 +2,9 @@
 from fastapi import FastAPI, Query, HTTPException
 import asyncio
 from datetime import datetime
-from .pipelines.youtube import fetch_youtube_comments
-from .utils.nlp_utils import analyze_comments
-from .m3_ideas import generate_m3
+from ._lib.pipelines.youtube import fetch_youtube_comments
+from ._lib.utils.nlp_utils import analyze_comments
+from ._lib.m3_ideas import generate_m3
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
